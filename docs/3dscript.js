6b18697632d9176892ea43271f7ecb552983857b
Carlos A  Lugo <kupkasmale@gmail.com>
2025-04-22 01:19:26 +0100

    WASM DEMOS

---
+++ docs/3dscript.js
@@ -0,0 +1,290 @@
+
+import * as THREE from 'three';
+import Stats from 'stats-gl';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+
+
+let container;
+let camera, scene, renderer;
+let gui;
+let stats;
+let A, B, plot_case='plane';
+let tf;
+let plane, planeGeometry,prt;
+let planepos,xp,yp,zp;
+let dotGeometry,DotGeo,pt;
+let status = false;
+let statusmsg; 
+let Rt,n;
+let play_button;
+let playflag = false;
+const playtime = 'Play t: ';
+
+
+let dotSolMat = new THREE.PointsMaterial(
+          {opacity:0.5, size: 0.025, color: 0xc1f211, transparent: true});
+
+let dotMaterial = new THREE.PointsMaterial({ size: 0.05, color: 0xff0000 });
+
+init()
+
+const pyodideRuntime = await loadPyodide();
+await pyodideRuntime.loadPackage("micropip");
+const micropip = pyodideRuntime.pyimport("micropip");
+await micropip.install("autograd")
+pyodideRuntime.runPython(await (await fetch("integrals.py")).text());
+
+let pyparaboloid = pyodideRuntime.globals.get('paraboloid');
+let pyplane = pyodideRuntime.globals.get('plane');
+let RK = pyodideRuntime.globals.get('RK');
+
+statusmsg = document.getElementById('info');
+statusmsg.innerText='Ready';
+
+//init();
+
+function init(){
+
+        A = 0.0;
+        B = 0.0;
+        xp = 0.95;
+        yp = -0.95;
+        zp = 0.0;
+        tf = 1.0;
+
+        container = document.getElementById( 'container' );
+				scene = new THREE.Scene();
+        scene.background = new THREE.Color( 0x222222 );
+				//scene.background = new THREE.Color( 0xf0f0f0 );
+				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10000 );
+
+				camera.position.set( 0, 2, 2 );
+				scene.add( camera );
+
+				scene.add( new THREE.AmbientLight( 0xf0f0f0, 3 ) );
+				const light = new THREE.SpotLight( 0xffffff, 4.5 );
+				light.position.set( 0, 1500, 200 );
+				light.angle = Math.PI * 0.2;
+				light.decay = 0;
+				light.castShadow = true;
+				light.shadow.camera.near = 200;
+				light.shadow.camera.far = 2000;
+				light.shadow.bias = - 0.000222;
+				light.shadow.mapSize.width = 1024;
+				light.shadow.mapSize.height = 1024;
+				scene.add( light );
+
+				planeGeometry = new THREE.PlaneGeometry( 3, 3 , 20, 20);
+				planeGeometry.rotateX( - Math.PI / 2 );
+				const planeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ffff, opacity: 0.2, side: THREE.DoubleSide, wireframe: true } );
+				//const planeMaterial = new THREE.MeshBasicMaterial( { color: 0x0000ff, opacity: 0.6, side: THREE.DoubleSide} );
+        plane = new THREE.Mesh( planeGeometry, planeMaterial );
+        planepos = planeGeometry.getAttribute('position');
+				plane.receiveShadow = true;
+				scene.add( plane );
+
+
+        dotGeometry = new THREE.BufferGeometry();
+        dotGeometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array([0,0,0]), 3));
+        
+        prt = new THREE.Points(dotGeometry, dotMaterial);
+        prt.position.x = xp;
+        prt.position.z = yp;
+        prt.position.y = zp;
+        scene.add(prt);
+
+        const axesHelper = new THREE.AxesHelper( 2 );
+        scene.add( axesHelper );
+
+	      renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.shadowMap.enabled = true;
+        document.body.appendChild( renderer.domElement );
+
+        // Controls
+				const controls = new OrbitControls( camera, renderer.domElement );
+				controls.damping = 0.2;
+				controls.addEventListener( 'change', render );
+
+        window.addEventListener( 'resize', onWindowResize );
+        onWindowResize();
+
+        initGui();
+        render();
+        
+}
+
+function rem_solution(){
+  let kn = 0;
+  while(kn<Rt.length){
+    let pn = scene.getObjectByName(kn);
+    scene.remove(pn);
+    kn++;
+  }
+  render();
+  playflag = false;
+}
+
+function add_solution(jn, rn){
+        DotGeo = new THREE.BufferGeometry();
+        DotGeo.setAttribute('position', new THREE.BufferAttribute(new Float32Array([0,0,0]), 3));
+        pt = new THREE.Points(dotGeometry, dotSolMat);
+        pt.position.x = rn[1];
+        pt.position.z = rn[2];
+        pt.position.y = rn[3];
+        pt.name = jn;
+        scene.add(pt);
+}
+
+function render() {
+
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  render();
+
+}
+
+
+function initGui(){
+
+  gui =  new GUI();
+
+  const param = {
+    'A':A,
+    'B':B,
+    'Case': 0,
+    'T': tf,
+    'Run': Runpy,
+    'Plot': Play
+  };
+
+
+  gui.add( param, 'Case', { 'plane': 0, 'paraboloid': 1 } ).onChange( function ( val ) {
+
+		switch ( val ) {
+				case 0:
+					//console.log('plane');
+          plot_case = 'plane';
+          if(playflag){rem_solution();}
+          transformplane(plot_case);
+					break;
+
+				case 1:
+					//console.log('paraboloid')
+          plot_case = 'paraboloid';
+          if(playflag){rem_solution();}
+          transformplane(plot_case);
+					break;
+        }
+				});
+
+  gui.add( param, 'A', 0.0, 5, 0.01 ).onChange( function ( val ) {
+    //console.log('A');
+    if(playflag){rem_solution();}
+    A = val;
+    transformplane(plot_case);
+    } );
+  
+  gui.add( param, 'B', 0.0, 5.0, 0.01 ).onChange( function ( val ) {
+    //console.log('B');
+    if(playflag){rem_solution();}
+    B = val;
+    transformplane(plot_case);
+    } );
+  
+
+    gui.add( param, 'T', 0.5, 50.0, 0.5 ).onChange( function ( val ) {
+    //console.log('A');
+     tf = val;
+    } );
+
+  gui.add(param,'Run');
+
+
+  play_button = gui.add(param,'Plot').disable();
+
+}
+
+function setIntTitle(title){
+  statusmsg.innerText = title;
+}
+
+
+function Runpy(){
+
+
+  if(playflag){rem_solution();}
+
+  xp = prt.position.x;
+  yp = prt.position.z;
+  zp = prt.position.y;
+  Rt = RK(tf,[xp,yp,zp],plot_case,1e-2,A,B);
+  play_button.disable(false);
+  console.log(Rt);
+
+}
+//function Runpy(){
+
+//  setIntTitle('Integrating');
+  
+ // if(playflag){rem_solution();}
+
+//  xp = prt.position.x;
+ // yp = prt.position.z;
+ // zp = prt.position.y;
+//  Rt = RK(tf,[xp,yp,zp],plot_case,1e-2,A,B);
+//  play_button.disable(false);
+//  console.log(Rt);
+//  setIntTitle('Done');
+//}
+
+
+function Play(){
+
+  if (playflag == false){
+    let jn =0;
+    while(jn<Rt.length){
+      add_solution(jn,Rt[jn])
+      jn++;
+    }
+    render();
+    playflag = true;
+    play_button.disable();
+
+  }
+
+}
+
+function transformplane(shape){
+  
+  const planepos = planeGeometry.getAttribute('position');
+  const vertex = new THREE.Vector3();
+  let  n ;
+
+  if (shape == 'paraboloid'){
+    n=2;
+    prt.position.y = pyparaboloid(A,B,xp,yp);
+  }
+
+  if (shape == 'plane'){
+    n=1;
+    prt.position.y = pyplane(A,B,xp,yp);
+  }
+
+  for (let i= 0; i < planepos.count;i++){
+      vertex.fromBufferAttribute( planepos, i ); // read vertex
+      vertex.y = A*vertex.x**n+B*vertex.z**n;//+B*vertex.y;
+      planepos.setXYZ( i, vertex.x, vertex.y, vertex.z ); // 
+      }
+  planepos.needsUpdate = true;
+  render();
+}
---
+++ docs/integrals.py
@@ -0,0 +1,76 @@
+import autograd.numpy as np 
+from autograd import grad
+from pyodide.ffi import to_js 
+
+def plane(a,b,x,y):
+    return a*x + b*y
+
+    
+def paraboloid(a,b,x,y):
+    return a*x*x + b*y*y
+
+
+def kn(r,pars,case):
+    a = pars[0]
+    b = pars[1]
+    g = 10
+    if case in ['plane','paraboloid']:
+        
+        if case == 'plane':
+            fn = plane
+        if case == 'paraboloid':
+            fn = paraboloid
+        
+        x = r[0]; y = r[1]; z = r[2]
+        vx = r[3]; vy =r[4]; vz= r[5]
+        
+        fx = grad(fn,2)(a,b,x,y)
+        fy = grad(fn,3)(a,b,x,y)
+        
+        dg = np.array([fx,fy,1])
+        dg2 = np.dot(dg,dg)
+        
+        fxx = grad(grad(fn,2),2)(a,b,x,y)
+        fyy = grad(grad(fn,3),3)(a,b,x,y)
+        ddg = np.array([fxx*vx*vx,fyy*vy*vy,g])
+        Num = np.sum(ddg)
+        Den = dg2
+        A = Num/dg2
+        
+        F = np.array([vx,vy,vz,-A*fx,-A*fy,-g+A])
+        
+        return F
+    
+
+def RK(tf,ro,case,h,a,b):
+
+    
+    dtp = 1e-3
+    tp = dtp
+    t = 0.0;
+    u = np.zeros(6)
+    u[0:3] = ro
+    prm = [a,b]
+
+    #tn = [0.0]
+
+    rn = []
+    rn.append([t,u[0],u[1],u[2]])
+
+    while t<tf:
+        k1 = kn(u,prm,case)
+        k2 = kn(u+0.5*h*k1,prm,case)
+        k3 = kn(u+0.5*h*k2,prm,case)
+        k4 = kn(u+h*k3,prm,case)
+        u = u+ (h/6.0)*(k1+2*k2+2*k3+k4)
+        t+=h
+
+        if t>tp:
+            #output_div.innerText = "Running"+str(t)
+            ##print(t)
+            rn.append([t,u[0],u[1],u[2]])
+        
+            tp+=dtp
+    #print(rn)
+    return to_js(rn)
+
